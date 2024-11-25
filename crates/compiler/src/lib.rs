c90dfa9c4160b67637fb2ac2a4801d7dc6813541
Saúl Cabrera <saulecabrera@gmail.com>
2024-11-25 18:52:27 -0500

    Remove unused variable

--- crates/compiler/src/lib.rs
+++ crates/compiler/src/lib.rs
@@ -1,6 +1,6 @@
 //! JAC - The Javy Ahead-of-Time Compiler.
 use anyhow::Result;
 
-pub fn compile(bytes: &[u8]) -> Result<Vec<u8>> {
+pub fn compile(_bytes: &[u8]) -> Result<Vec<u8>> {
     Ok(vec![])
 }
